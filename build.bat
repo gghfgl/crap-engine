e9116121d097dbd907b0aca1dcb87c17d5882e29
glab <glabordeh@gmail.com>
2020-03-23 15:06:31 +0100

    add dependencies

--- build.bat
+++ build.bat
@@ -12,8 +12,8 @@ REM  -W4 -wd4201 -wd4100 -wd4505 -wd4189
 set CommonCompilerFlags=/nologo /EHa -FC -WX -W4 -wd4201 -wd4100 -wd4505 -wd4189 -MTd -Oi -Od -GR -Zi
 set CommonLinkerFlags=-incremental:no -opt:ref user32.lib gdi32.lib shell32.lib opengl32.lib glfw3.lib
 
-set AdditionalIncludesDir=/I C:\Users\test\Documents\dev\opengl\includes
-set AdditionalLibDir=/LIBPATH:C:\Users\test\Documents\dev\opengl\lib
+set AdditionalIncludesDir=/I C:\Users\test\Documents\dev\crapengine\dep\includes
+set AdditionalLibDir=/LIBPATH:C:\Users\test\Documents\dev\crapengine\dep\lib
 
 REM robocopy ..\Shaders Shaders /MIR
 REM robocopy ..\data data /MIR
