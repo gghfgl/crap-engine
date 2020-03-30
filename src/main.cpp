7012545cddd5423a4226b9a8286e10442aea44cb
glab <glabordeh@gmail.com>
2020-03-30 09:38:26 +0200

    simple object picking test

--- src/main.cpp
+++ src/main.cpp
@@ -22,14 +22,17 @@
 */
 
 // TODO:
+// * stencil test outline object + picking object by color
+// * move object from slot to slot
+// * add click action to container object and open imgui inventory?
+// * move object between inventories
 // * implement frame rate counter ms VSYNC ?
 // * logger ?
 // * implement game entity and level?
 // * in fuction time profiler (handemade hero fast thread id retrieval)
 // * work on z-buffer to avoid z-fighting colors/ textures
 // * add texture and white default texture to batch rendering cube.
-// * generate terrain?
-// * stencil test outline object
+// * generate terrain (advanced)?
 // * compile and read shader for presetting uniform? the cherno
 // * load models assimp?
 // * batch rendering models?
@@ -37,6 +40,7 @@
 // * light system PBR?
 
 int GetIndexByColor(int r, int g, int b);
+glm::vec4 GetColorByIndex(int index);
 void CrapColors(float *r, float *g, float *b);
 void DrawTerrain(engine *Engine, int mapSize);
 void DrawContainer(engine *Engine, float scale);
@@ -88,9 +92,11 @@ int main(int argc, char *argv[])
 	ResetRendererStats(Engine->Renderer);
         StartRendering(Engine->Renderer, Engine->Camera);
 
+	BYTE bArray[4];
+	int invertMouseY = 960 - (int)Engine->InputState->MousePosY;
 	// NOTE: Draw terrain with slider window
 	ImGui::SetNextWindowPos(ImVec2(10, 150));
-	ImGui::SetNextWindowSize(ImVec2(200, 150));
+	ImGui::SetNextWindowSize(ImVec2(200, 220));
 	ImGui::Begin("data", nullptr, ImGuiWindowFlags_NoResize);
 	noWindowFocus = !ImGui::IsWindowFocused();
 	ImGui::Text("Terrain:");
@@ -99,7 +105,9 @@ int main(int argc, char *argv[])
 	ImGui::Separator();
 	ImGui::Text("Picking:");
 	ImGui::Separator();
-	ImGui::Text("PixelColor= %d", iResult);
+	ImGui::Text("mX= %d /mYinvert %d", (int)Engine->InputState->MousePosX, invertMouseY);
+	ImGui::Text("r=%d g=%d b=%d", bArray[0], bArray[1], bArray[2]);
+	ImGui::Text("bitValue= %d", iResult);
 	ImGui::End();
 
 	DrawTerrain(Engine, MAPSIZE);
@@ -113,27 +121,24 @@ int main(int argc, char *argv[])
 
 
 	{ // NOTE: Stencil rendering
-	    // TODO: picking right color
-	    BYTE bArray[4];
-	    glReadPixels((int)Engine->InputState->MousePosX, (int)Engine->InputState->MousePosY,
-			 1, 1, GL_RGB, GL_UNSIGNED_BYTE, bArray);
-	    iResult = GetIndexByColor(bArray[0], bArray[1], bArray[2]);
-	    if(iResult == RGB_WHITE)
-	    {
-		std::cout << "TESTEUUUUUUUU" << std::endl;
-	    }
-
 	    glStencilFunc(GL_ALWAYS, 1, 0xFF);
 	    glStencilMask(0xFF);
 
 	    DrawContainer(Engine, 1.0f);
 
-	    StartRenderingStencil(Engine->Renderer, Engine->Camera);
-	    glStencilFunc(GL_NOTEQUAL, 1, 0xFF);
-	    glStencilMask(0x00);
-	    glDisable(GL_DEPTH_TEST);
+	    // TODO: picking right color
+	    glReadPixels((int)Engine->InputState->MousePosX, invertMouseY,
+			 1, 1, GL_RGB, GL_UNSIGNED_BYTE, bArray);
+	    iResult = GetIndexByColor(bArray[0], bArray[1], bArray[2]);
+	    if(iResult == RGB_WHITE)
+	    {	    
+		StartRenderingStencil(Engine->Renderer, Engine->Camera);
+		glStencilFunc(GL_NOTEQUAL, 1, 0xFF);
+		glStencilMask(0x00);
+		glDisable(GL_DEPTH_TEST);
 
-	    DrawContainer(Engine, 1.1f);
+		DrawContainer(Engine, 1.1f);
+	    }
 
 	    StopRenderingStencil();
 	}
@@ -148,6 +153,15 @@ int main(int argc, char *argv[])
 int GetIndexByColor(int r, int g, int b)
 {
    return (r)|(g<<8)|(b<<16);
+}
+
+glm::vec4 GetColorByIndex(int index)
+{
+   int r = index&0xFF;
+   int g = (index>>8)&0xFF;
+   int b = (index>>16)&0xFF;
+
+   return glm::vec4(float(r)/255.0f, float(g)/255.0f, float(b)/255.0f, 1.0f);
 }
 
 void CrapColors(float *r, float *g, float *b) {
