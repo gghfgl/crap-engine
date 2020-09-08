eb3216f11f46fc49373f6feb7ff5dcdeb3a66312
glab <glabexp@gmail.com>
2020-09-09 00:43:24 +0200

    clean up and not so proud

---
+++ src/unity_build.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <windows.h>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include "IMGUI/imgui.h"
+#include "IMGUI/imgui_impl_glfw.h"
+#include "IMGUI/imgui_impl_opengl3.h"
+
+#include "../dep/src/stb_image.cpp"
+#include "../dep/src/glad.c"
+#include "../dep/src/IconsFontAwesome5.h"
+
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+
+#include "intrinsic.h"
+#include "input.cpp"
+#include "camera.cpp"
+#include "shader.cpp"
+// #include "texture.cpp"
+#include "mesh.cpp"
+#include "ray.cpp"
+#include "renderer.cpp"
+#include "window.cpp"
+#include "gui.h"
