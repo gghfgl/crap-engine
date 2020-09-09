282cb93b7b7a2cca91d76729b76cc0ca0d1e0f17
glab <glabexp@gmail.com>
2020-09-09 14:04:47 +0200

    fix generate terrain coordinate

--- src/Engine/entity.cpp
+++ src/Engine/entity.cpp
@@ -1,6 +1,6 @@
 #include "entity.h"
 
-static glm::mat4* generate_terrain_model_matrices(uint32 squareSideLenght);
+static glm::mat4* generate_terrain_model_matrices(uint32 sideLenght);
 static uint32 load_cubemap_texture_from_file(std::vector<std::string> faces);
 
 void Delete(entity_t *Entity)
@@ -27,35 +27,6 @@ void Delete(terrain_t *Terrain)
     Delete(Terrain->Entity);
     delete []Terrain->ModelMatrices;
     delete Terrain;
-}
-
-static glm::mat4* generate_terrain_model_matrices(uint32 squareSideLenght)
-{
-    glm::mat4 *modelMatrices;
-    modelMatrices = new glm::mat4[squareSideLenght * squareSideLenght];    
-    glm::vec3 size = { 1.0f, 1.0f, 1.0f };
-    float posX = 0.0f;
-
-    uint32 index = 0;
-    for (uint32 i = 0; i < squareSideLenght; i++)
-    {
-        float posZ = -size.z;
-        for (uint32 y = 0; y < squareSideLenght; y++)
-        {
-            glm::mat4 model = glm::mat4(1.0f);
-            model = glm::translate(model, { posX, 0.0f, posZ });
-            model = glm::scale(model, glm::vec3(1.0f));
-            model = glm::rotate(model, glm::radians(0.0f),
-                                glm::vec3(0.0f, 1.0f, 0.0f));
-            modelMatrices[index] = model;
-
-            posZ -= size.z;
-            index++;
-        }
-        posX += size.x;
-    }
-
-    return modelMatrices;
 }
 
 skybox_t* GenerateSkyboxFromFiles(std::vector<std::string> faces)
@@ -274,6 +245,35 @@ int32 OpenSceneFromTextFormat(char *filepath, std::map<uint32, entity_t*> *Scene
     // DEBUG:
     printf("opened: %s", filepath);
     return 0;
+}
+
+static glm::mat4* generate_terrain_model_matrices(uint32 sideLenght)
+{
+    glm::mat4 *modelMatrices;
+    modelMatrices = new glm::mat4[sideLenght * sideLenght];    
+    glm::vec3 size = { 1.0f, 1.0f, 1.0f };
+    float32 posX = -((float32)sideLenght / 2.0f - 0.5f);
+
+    uint32 index = 0;
+    for (uint32 i = 0; i < sideLenght; i++)
+    {
+        float32 posZ = ((float32)sideLenght / 2.0f - 0.5f);
+        for (uint32 y = 0; y < sideLenght; y++)
+        {
+            glm::mat4 model = glm::mat4(1.0f);
+            model = glm::translate(model, { posX, 0.0f, posZ });
+            model = glm::scale(model, glm::vec3(1.0f));
+            model = glm::rotate(model, glm::radians(0.0f),
+                                glm::vec3(0.0f, 1.0f, 0.0f));
+            modelMatrices[index] = model;
+
+            posZ -= size.z;
+            index++;
+        }
+        posX += size.x;
+    }
+
+    return modelMatrices;
 }
 
 static uint32 load_cubemap_texture_from_file(std::vector<std::string> faces)
--- src/Engine/entity.h
+++ src/Engine/entity.h
@@ -8,7 +8,7 @@ struct entity_t
     mesh_t *PickingSphere;
     glm::vec3 Position = glm::vec3(0.0f, 0.0f, 0.0f);
     float32 Scale = 1.0f;
-    float32 Rotate = 0.0f; // degeres
+    float32 Rotate = 0.0f; // degres
 };
 
 // ======================================
@@ -16,7 +16,7 @@ struct entity_t
 struct terrain_t
 {
     entity_t *Entity;
-    uint32 SideLenght = 10; // default size
+    uint32 SideLenght = 6; // default size
     bool IsGenerated = false;
     glm::mat4 *ModelMatrices;
 };
