ad154ea0ad09e54e43716de5aabb7459e2a6fa6f
glab <glabexp@gmail.com>
2020-12-18 00:19:57 +0100

    cleanup editor useless code

--- src/editor.h
+++ src/editor.h
@@ -1,16 +1,5 @@
 #pragma once
 
-struct editor_t
-{
-	bool Active;
-     Mesh *MeshGrid; // TODO: reference grid
-     Mesh *MeshAxisDebug;
-     Mesh *MeshRay;
-	uint32 GridResolution;
-     Skybox *skybox; // TODO: add skybox load from file in panel with min
-	bool ShowSkybox;
-};
-
 struct GlobalState
 {
     bool active = true;
